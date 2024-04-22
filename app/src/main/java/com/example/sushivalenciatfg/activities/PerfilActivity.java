3864cdd949e7257e1b5a0b3255f4dde800685b36
martaesc <escorihuela_m246@ieseduardoprimo.es>
2024-04-22 20:00:49 +0200

    Documentación Javadoc para Perfil Activity

--- app/src/main/java/com/example/sushivalenciatfg/activities/PerfilActivity.java
+++ app/src/main/java/com/example/sushivalenciatfg/activities/PerfilActivity.java
@@ -37,18 +37,22 @@ import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+/**
+ * Esta es la clase PerfilActivity, que extiende AppCompatActivity.
+ * Se encarga de gestionar el perfil del usuario.
+ */
 public class PerfilActivity extends AppCompatActivity {
 
+    // Referencias a los elementos de la vista
     private TextInputLayout lyNombreUsuario;
     private TextInputLayout lyEmail;
     private TextView tvcontrasena;
     private TextInputLayout lyTipoUsuario;
-
     private CircleImageView ivfotoPerfil;
-
     private Button btnGuardarCambios;
     private Button btnVolver;
 
+    // Referencias a FirebaseAuth, FirebaseFirestore y FirebaseUser
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
@@ -57,10 +61,11 @@ public class PerfilActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> mGalleryResultLauncher;
     private ActivityResultLauncher<Intent> mCameraResultLauncher;
 
+
+    // Getters y setters
     public void setFirebaseAuth(FirebaseAuth mAuth) {
         this.mAuth = mAuth;
     }
-
 
     public void setCurrentUser(FirebaseUser currentUser) {
         this.currentUser = currentUser;
@@ -78,6 +83,15 @@ public class PerfilActivity extends AppCompatActivity {
         return lyTipoUsuario;
     }
 
+
+    /**
+     * Este método se llama cuando la actividad está iniciando.
+     * Inicializa la actividad y establece los onClickListener para los botones.
+     *
+     * @param savedInstanceState Si la actividad se está reinicializando después de haber sido cerrada previamente
+     *                           entonces este Bundle contiene los datos que suministró más recientemente en onSaveInstanceState(Bundle).
+     *                           Nota: De lo contrario, es nulo.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,53 +111,59 @@ public class PerfilActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Este método se encarga de obtener las referencias a los elementos de la vista.
+     */
     public void obtenerReferencias() {
         lyNombreUsuario = findViewById(R.id.usernameInputLayout);
         lyEmail = findViewById(R.id.emailInputLayout);
         tvcontrasena = findViewById(R.id.tvContrasena);
         lyTipoUsuario = findViewById(R.id.userTypeInputLayout);
-
         ivfotoPerfil = findViewById(R.id.ivPhotoProfile);
-
         btnGuardarCambios = findViewById(R.id.saveButton);
         btnVolver = findViewById(R.id.backButton);
     }
 
-   public void inicializarActivityResultLaunchers() {
-    // Inicialización de los ActivityResultLauncher para la galería y la cámara
-    mGalleryResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    Intent data = result.getData();
-                    if (data != null) {
-                        Uri uri = data.getData();
-                        ivfotoPerfil.setImageURI(uri);
-                    } else {
-                        Toast.makeText(this, "Error al obtener la imagen de la galería", Toast.LENGTH_SHORT).show();
+
+    /**
+     * Este método se encarga de inicializar los ActivityResultLauncher para la galería y la cámara.
+     */
+    public void inicializarActivityResultLaunchers() {
+        mGalleryResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri uri = data.getData();
+                            ivfotoPerfil.setImageURI(uri);
+                        } else {
+                            Toast.makeText(this, "Error al obtener la imagen de la galería", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
-            }
-    );
-
-    mCameraResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    Intent data = result.getData();
-                    if (data != null) {
-                        Bundle extras = data.getExtras();
-                        Bitmap imageBitmap = (Bitmap) extras.get("data");
-                        ivfotoPerfil.setImageBitmap(imageBitmap);
-                    } else {
-                        Toast.makeText(this, "Error al obtener la imagen de la cámara", Toast.LENGTH_SHORT).show();
+        );
+        mCameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Bundle extras = data.getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ivfotoPerfil.setImageBitmap(imageBitmap);
+                        } else {
+                            Toast.makeText(this, "Error al obtener la imagen de la cámara", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
-            }
-    );
-}
+        );
+    }
 
-    // Método para abrir el diálogo de selección de imagen
+
+    /**
+     * Este método se encarga de abrir el diálogo de selección de imagen.
+     */
     public void abrirDialogoSeleccionImagen() {
         ivfotoPerfil.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -162,16 +182,17 @@ public class PerfilActivity extends AppCompatActivity {
         });
     }
 
-    // Método para obtener los datos del usuario actual
-    // (al estar guardando el nombre de usuario en Firestore cuando el usuario se registra, debo buscarlo Firestore en lugar de FirebaseUser,
-    // ya que en este ultimo solamente se guarda el email y la contraseña)
+
+    /**
+     * Este método se encarga de obtener los datos del usuario actual.
+     */
     public void obtenerDatosUsuario() {
         currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             String email = currentUser.getEmail();
             lyEmail.getEditText().setText(email);
 
-            // Para obtener el nombre de usuario, el tipo de usuario y la URL de la imagen, necesitamos leerlos de Firestore
+            // Para obtener el nombre de usuario, el tipo de usuario y la URL de la imagen de perfil, necesitamos leerlos de Firestore
             db.collection("usuarios")
                     .whereEqualTo("uid", currentUser.getUid())
                     .get()
@@ -181,11 +202,10 @@ public class PerfilActivity extends AppCompatActivity {
                                 String nombreUsuario = document.getString("nombreUsuario");
                                 Log.d("Firestore", "Nombre de usuario obtenido: " + nombreUsuario);
                                 lyNombreUsuario.getEditText().setText(nombreUsuario);
-
                                 String tipoUsuario = document.getString("tipoUsuario");
                                 lyTipoUsuario.getEditText().setText(tipoUsuario);
 
-                                // Comprobar si el usuario tiene una imagen de perfil
+                                // Comprobamos si el usuario tiene una imagen de perfil
                                 if (document.contains("fotoPerfil") && !document.getString("fotoPerfil").isEmpty()) {
                                     String urlImagen = document.getString("fotoPerfil");
                                     // Cargamos la imagen en el CircleImageView usando Glide
@@ -193,7 +213,7 @@ public class PerfilActivity extends AppCompatActivity {
                                             .load(urlImagen)
                                             .into(ivfotoPerfil);
                                 } else {
-                                    // Establecer la imagen por defecto
+                                    // sino establecemos la imagen por defecto
                                     ivfotoPerfil.setImageResource(R.drawable.foto_perfil_defecto);
                                 }
                             }
@@ -206,6 +226,10 @@ public class PerfilActivity extends AppCompatActivity {
         }
     }
 
+
+    /**
+     * Este método se encarga de actualizar los datos del usuario.
+     */
     public void actualizarDatosUsuario() {
         String nombreUsuario = lyNombreUsuario.getEditText().getText().toString();
         String correo = lyEmail.getEditText().getText().toString();
@@ -217,15 +241,16 @@ public class PerfilActivity extends AppCompatActivity {
             return;
         }
 
-        // Comprobar si el nombre de usuario ya existe y no pertenece al usuario actual (para evitar que salte el error por tener el propio usuario ya ese nombre)
+        // Comprobar si el nombre de usuario ya pertenece a alguien que no sea ek usuario actual (para evitar que salte el error por tener el propio usuario ya ese nombre)
         db.collection("usuarios")
                 .whereEqualTo("nombreUsuario", nombreUsuario)
                 .whereNotEqualTo("uid", currentUser.getUid())
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
+                        //si el nombre de usuario no está en uso por otro usuario
                         if (task.getResult().isEmpty()) {
-                            // El nombre de usuario no existe o pertenece al usuario actual, continuar con la actualización
+                            //si ha seleccionado una imagen, actualizar el usuario con la imagen
                             if (ivfotoPerfil.getDrawable() != null) {
                                 actualizarUsuarioConImagen(nombreUsuario, correo, tipoUsuario);
                             } else {
@@ -233,9 +258,8 @@ public class PerfilActivity extends AppCompatActivity {
                                 actualizarUsuarioEnFirestore(nombreUsuario, correo, tipoUsuario, null);
                             }
 
-                            // Comprobar si el correo electrónico ha sido modificado
+                            // si el correo es diferente al actual, actualizar el correo en FirebaseAuth
                             if (!correo.equals(currentUser.getEmail())) {
-                                // Llamar al método para actualizar el correo electrónico en FirebaseAuth
                                 actualizarCorreoEnAuth(correo);
                             }
 
@@ -249,18 +273,24 @@ public class PerfilActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * Este método se encarga de actualizar el usuario con una imagen.
+     *
+     * @param nombreUsuario El nombre de usuario ingresado por el usuario.
+     * @param correo El correo electrónico ingresado por el usuario.
+     * @param tipoUsuario El tipo de usuario seleccionado por el usuario.
+     */
     public void actualizarUsuarioConImagen(String nombreUsuario, String correo, String tipoUsuario) {
-        // Convertir la imagen en un Bitmap
+        // Convertimos la imagen en un Bitmap para poder comprimirla
         BitmapDrawable drawable = (BitmapDrawable) ivfotoPerfil.getDrawable();
         Bitmap fotoPerfil = drawable.getBitmap();
 
-        // Comprimir la imagen en un ByteArrayOutputStream
+        // Comprimimos la imagen en un ByteArrayOutputStream (formato requerido por el método putBytes() de Firebase Storage para subir la imagen)
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         fotoPerfil.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] datosImagen = baos.toByteArray();
 
-        // Subir la imagen a Firebase Storage y obtener la URL
+        // Subimos la imagen a Firebase Storage y obtenemos su URL
         StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("imagenes_usuario/" + UUID.randomUUID().toString() + ".jpg");
         storageRef.putBytes(datosImagen)
                 .addOnSuccessListener(taskSnapshot -> storageRef.getDownloadUrl()
@@ -278,7 +308,17 @@ public class PerfilActivity extends AppCompatActivity {
                 });
     }
 
+
+    /**
+     * Este método se encarga de actualizar el usuario en Firestore.
+     *
+     * @param nombreUsuario El nombre de usuario ingresado por el usuario.
+     * @param correo El correo electrónico ingresado por el usuario.
+     * @param tipoUsuario El tipo de usuario seleccionado por el usuario.
+     * @param urlImagen La URL de la imagen del usuario.
+     */
     public void actualizarUsuarioEnFirestore(String nombreUsuario, String correo, String tipoUsuario, String urlImagen) {
+        //creamos un map con los datos del usuario a actualizar en Firestore
         Map<String, Object> usuario = new HashMap<>();
         usuario.put("nombreUsuario", nombreUsuario);
         usuario.put("correo", correo);
@@ -287,21 +327,21 @@ public class PerfilActivity extends AppCompatActivity {
             usuario.put("fotoPerfil", urlImagen);
         }
 
-        // Buscar el documento donde el campo 'uid' es igual al UID del usuario actual
+        // Buscamos el documento donde el campo 'uid' es igual al UID del usuario actual
         db.collection("usuarios")
                 .whereEqualTo("uid", currentUser.getUid())
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful() && !task.getResult().isEmpty()) {
-                        // Obtener el ID del primer documento que coincide con la consulta
+                        // Obtenemos el ID del primer documento que coincide con la consulta
                         String documentId = task.getResult().getDocuments().get(0).getId();
 
-                        // Actualizar el documento con el ID obtenido
+                        // y actualizamos el documento con el ID obtenido
                         db.collection("usuarios").document(documentId)
                                 .update(usuario)
                                 .addOnSuccessListener(aVoid -> {
                                     Toast.makeText(this, "Usuario actualizado con éxito", Toast.LENGTH_SHORT).show();
-                                    // Recargar los datos del usuario en la interfaz
+                                    // Refrescamos los datos del usuario en la interfaz
                                     obtenerDatosUsuario();
 
                                 })
@@ -316,8 +356,11 @@ public class PerfilActivity extends AppCompatActivity {
     }
 
 
-
-    // Método para actualizar el correo electrónico en FirebaseAuth
+    /**
+     * Este método se encarga de actualizar el correo electrónico en FirebaseAuth.
+     *
+     * @param correo El correo electrónico ingresado por el usuario.
+     */
     public void actualizarCorreoEnAuth(String correo) {
         // Enviar el correo de verificación al nuevo correo
         currentUser.verifyBeforeUpdateEmail(correo)
@@ -325,7 +368,7 @@ public class PerfilActivity extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         Toast.makeText(this, "Correo de verificación enviado. Por favor, revise su bandeja de entrada.", Toast.LENGTH_LONG).show();
 
-                        // Iniciar LoginActivity para que inicie sesión con el nuevo correo
+                        // Iniciamos LoginActivity para que inicie sesión con el nuevo correo
                         Intent intent = new Intent(this, LoginActivity.class);
                         startActivity(intent);
                         finish();
@@ -337,7 +380,9 @@ public class PerfilActivity extends AppCompatActivity {
     }
 
 
-    // Método para enviar un correo para cambiar la contraseña
+    /**
+     * Este método se encarga de enviar un correo para cambiar la contraseña.
+     */
     public void enviarCorreoCambioContrasena() {
         mAuth.sendPasswordResetEmail(currentUser.getEmail())
                 .addOnCompleteListener(task -> {
@@ -354,6 +399,10 @@ public class PerfilActivity extends AppCompatActivity {
                 });
     }
 
+
+    /**
+     * Este método se encarga de volver a la actividad principal.
+     */
     public void volver() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
