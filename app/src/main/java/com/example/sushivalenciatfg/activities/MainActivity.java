79a6901e1f1d13e2ac7e74827c8483aabc7a544a
martaesc <escorihuela_m246@ieseduardoprimo.es>
2024-04-23 19:01:16 +0200

    cambios configuración búsqueda search view y javadoc MainActivity

--- app/src/main/java/com/example/sushivalenciatfg/activities/MainActivity.java
+++ app/src/main/java/com/example/sushivalenciatfg/activities/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.sushivalenciatfg.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,33 +16,42 @@ import android.widget.Toast;
 import com.example.sushivalenciatfg.R;
 import com.example.sushivalenciatfg.adapters.RestauranteAdapter;
 import com.example.sushivalenciatfg.models.Restaurante;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta es la clase MainActivity, que extiende AppCompatActivity.
+ * Se encarga de gestionar la pantalla principal de la aplicación.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // Referencias a los elementos de la vista
     private ImageButton btnPerfil;
     private ImageButton btnSalir;
     private SearchView searchView;
     private Button btnAñadirRestaurante;
     private RecyclerView recyclerView;
+
+    // Adaptador para el RecyclerView
     private RestauranteAdapter adapter;
+
+    // Lista de restaurantes
     private List<Restaurante> restaurantes;
+
+    // Referencias a FirebaseAuth, FirebaseFirestore y FirebaseUser
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
-
     private FirebaseUser currentUser;
 
-    //setters útiles para las pruebas, ya que permiten inyectar instancias simuladas de FirebaseAuth y FirebaseFirestore en LoginActivity.
+
+    //setters útiles para las pruebas (permiten inyectar instancias simuladas de FirebaseAuth, FirebaseFirestore y FirebaseUser)
     public void setFirebaseAuth(FirebaseAuth mAuth) {
         this.mAuth = mAuth;
     }
@@ -58,36 +65,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * Este método se llama cuando la actividad está iniciando.
+     * Inicializa la actividad y establece los onClickListener para los botones.
+     *
+     * @param savedInstanceState Si la actividad se está reinicializando después de haber sido cerrada previamente
+     *                           entonces este Bundle contiene los datos que suministró más recientemente en onSaveInstanceState(Bundle).
+     *                           Nota: De lo contrario, es nulo.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //inicializar instancia de Firebase
         db = FirebaseFirestore.getInstance();
-
         mAuth = FirebaseAuth.getInstance();
 
         obtenerReferencias();
-
         obtenerRestaurantes();
-
         visibilidadBotonAñadirRestaurante();
-
-        configurarBusqueda();
-
+        configurarBusquedaEnSearchView();
 
         btnAñadirRestaurante.setOnClickListener(v -> añadirRestaurante(v));
-
         btnPerfil.setOnClickListener(v -> irPerfil(v));
-
         btnSalir.setOnClickListener(v -> salir(v));
-
-
     }
 
 
+    /**
+     * Este método se encarga de obtener las referencias a los elementos de la vista.
+     */
     public void obtenerReferencias() {
         btnPerfil = findViewById(R.id.btnPerfil);
         btnSalir = findViewById(R.id.btnSalir);
@@ -96,60 +103,104 @@ public class MainActivity extends AppCompatActivity {
         btnAñadirRestaurante = findViewById(R.id.btnAñadirRestaurante);
     }
 
-    // configurar el SearchView
-    public void configurarBusqueda() {
+
+    /**
+     * Este método se encarga de configurar el SearchView para que realice la búsqueda de restaurantes.
+     */
+    public void configurarBusquedaEnSearchView() {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //se llama cuando el usuario envía la consulta de búsqueda al presionar el botón de búsqueda en el teclado
             @Override
             public boolean onQueryTextSubmit(String query) {
-                buscarRestaurante(query);
+                if (!query.isEmpty()) {
+                    buscarRestaurante(query);
+                } else {
+                    // Restablecer la lista de restaurantes a su estado original
+                    adapter = new RestauranteAdapter(restaurantes, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                }
                 return true;
             }
 
+            //se llama cuando el texto de la consulta de búsqueda cambia (los resultados de la búsqueda se actualizan en tiempo real a medida que el usuario escribe)
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                if (!newText.isEmpty()) {
+                    buscarRestaurante(newText);
+                } else {
+                    adapter = new RestauranteAdapter(restaurantes, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                }
+                return true;
             }
         });
     }
 
+
+    /**
+     * Este método se encarga de la lógica de buscar un restaurante por su nombre.
+     *
+     * @param nombre El nombre del restaurante a buscar.
+     */
+    public void buscarRestaurante(String nombre) {
+        List<Restaurante> resultadosBusqueda = new ArrayList<>();
+        for (Restaurante restaurante : restaurantes) {
+            if (restaurante.getNombre().toLowerCase().contains(nombre.toLowerCase())) {
+                resultadosBusqueda.add(restaurante);
+            }
+        }
+
+        if (resultadosBusqueda.isEmpty()) {
+            Toast.makeText(MainActivity.this, "No se encontraron restaurantes que coincidan con la búsqueda", Toast.LENGTH_SHORT).show();
+        } else {
+            // Actualizar el RecyclerView con los resultados de la búsqueda
+            adapter = new RestauranteAdapter(resultadosBusqueda, MainActivity.this);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+
+    /**
+     * Este método se encarga de obtener todos los restaurantes de la base de datos Firestore.
+     */
     public void obtenerRestaurantes() {
         db.collection("restaurantes")
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            restaurantes = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Restaurante restaurante = document.toObject(Restaurante.class);
-                                restaurantes.add(restaurante);
-                            }
-                            // Configurar el RecyclerView
-                            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                            adapter = new RestauranteAdapter(restaurantes, MainActivity.this);
-                            recyclerView.setAdapter(adapter);
-                        } else {
-                            Log.d("MainActivity", "Error obteniendo restaurantes: ", task.getException());
-                            Toast.makeText(MainActivity.this, "Error obteniendo restaurantes", Toast.LENGTH_SHORT).show();
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //si la tarea se completó con éxito: obtenemos los restaurantes, los convertimos de documento a restaurante y los añadimos a la lista
+                        restaurantes = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Restaurante restaurante = document.toObject(Restaurante.class);
+                            restaurantes.add(restaurante);
                         }
+                        // mostramos en el RecyclerView la lista de restaurantes utilizando el RestauranteAdapter para llenarlo
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                        adapter = new RestauranteAdapter(restaurantes, MainActivity.this);
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        Log.d("MainActivity", "Error obteniendo restaurantes: ", task.getException());
+                        Toast.makeText(MainActivity.this, "Error obteniendo restaurantes", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
+
+    /**
+     * Este método se encarga de controlar la visibilidad  del botón de añadir restaurante en la interfaz en función del tipo de usuario que ha iniciado sesión.
+     */
     public void visibilidadBotonAñadirRestaurante() {
         currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            // Si el usuario ha iniciado sesión, obtener su ID de usuario
             String userId = currentUser.getUid();
-            // Buscar en la colección "usuarios" un documento donde el campo "uid" coincide con el ID del usuario
+            // Buscamos en la colección "usuarios" un documento donde el campo "uid" coincide con el ID del usuario actual
             db.collection("usuarios")
                     .whereEqualTo("uid", userId)
                     .get()
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful() && !task.getResult().isEmpty()) {
-                            // Obtener el tipo de usuario del documento
+                            // Obtenemos el tipo de usuario que es consultando el campo "tipoUsuario" del documento
                             String tipoUsuario = task.getResult().getDocuments().get(0).getString("tipoUsuario");
-                            // Mostrar u ocultar el botón de añadir restaurante según el tipo de usuario
                             if ("Restaurante".equals(tipoUsuario)) {
                                 btnAñadirRestaurante.setVisibility(View.VISIBLE);
                             } else {
@@ -166,29 +217,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void buscarRestaurante(String nombre) {
-        List<Restaurante> resultadosBusqueda = new ArrayList<>();
-        for (Restaurante restaurante : restaurantes) {
-            if (restaurante.getNombre().toLowerCase().contains(nombre.toLowerCase())) {
-                resultadosBusqueda.add(restaurante);
-            }
-        }
-        // Actualizar el RecyclerView con los resultados de la búsqueda
-        adapter = new RestauranteAdapter(resultadosBusqueda, MainActivity.this);
-        recyclerView.setAdapter(adapter);
-    }
 
+    /**
+     * Este método se encarga de eliminar un restaurante, tanto de la base de datos como de la interfaz.
+     *
+     * @param restauranteId El ID del restaurante a eliminar.
+     */
     public void eliminarRestaurante(String restauranteId) {
         currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             String userId = currentUser.getUid();
+            // Buscamos el restaurante en la colección "restaurantes" con el ID del restaurante a eliminar
             db.collection("restaurantes")
                     .document(restauranteId)
                     .get()
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             DocumentSnapshot document = task.getResult();
+                            //si el restaurante (documento) existe en la bbdd, comprobamos si el usuario actual es su creador
                             if (document.exists()) {
+                                // comparando el ID del usuario actual con el campo "idUsuarioRestaurante" del documento
                                 String creadorId = document.getString("idUsuarioRestaurante");
                                 if (userId.equals(creadorId)) {
                                     db.collection("restaurantes").document(restauranteId)
@@ -221,23 +269,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //añadir restaurante
+
+    /**
+     * Este método se encarga de abrir la pantalla para añadir un nuevo restaurante.
+     *
+     * @param view La vista que fue clickeada.
+     */
     public void añadirRestaurante(View view) {
         Intent intent = new Intent(this, NuevoRestauranteActivity.class);
         startActivity(intent);
     }
 
-    //para ir al perfil
+
+    /**
+     * Este método se encarga de ir a la pantalla del perfil del usuario.
+     *
+     * @param view La vista que fue clickeada.
+     */
     public void irPerfil(View view) {
         Intent intent = new Intent(this, PerfilActivity.class);
         startActivity(intent);
     }
 
-    //salir
+
+    /**
+     * Este método se encarga de salir de la aplicación.
+     *
+     * @param view La vista que fue clickeada.
+     */
     public void salir(View view) {
         finishAffinity();
     }
-
 
 
 }
